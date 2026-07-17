da27d29f49a96c862ceedb405c66a45a0ad3c2b8
eulcau <lx_0318@mail.ustc.edu.cn>
2026-07-18 04:03:38 +0800

    feat(embed): export character editing helpers

--- embed.ts
+++ embed.ts
@@ -4,9 +4,20 @@ export {
 } from './components/AutoCharacterBuilder';
 export { CharacterSheet } from './components/CharacterSheet';
 export { Header } from './components/Header';
+export {
+  default as SearchPanel,
+  type SearchableFeature,
+  type SearchableMagicItem,
+  type SearchableSpell,
+} from './components/SearchPanel';
 export { LanguageProvider } from './contexts/LanguageContext';
 export { INITIAL_CHARACTER, type CharacterData } from './types';
 export {
   normalizeCharacter,
   serializeCharacter,
 } from './utils/characterStorage';
+export {
+  applyCharacterAdjustments,
+  removeCharacterAdjustments,
+} from './utils/characterAdjustments';
+export { refreshCharacterAutomation } from './utils/equipmentRules';
